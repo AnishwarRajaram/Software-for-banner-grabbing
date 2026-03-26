84f509830a6a970666cd1f009c7d13ff5eb77dfa
AnishwarRajaram <rajaramanishwar@gmail.com>
2026-03-27 00:46:53 +0530

    PESMap.py multiserver implementation with a few bug fixes in HTTPEngine.py
    working ftp and http(s) implementations now in PESMap.py

---
+++ PESMap.py
@@ -0,0 +1,34 @@
+import HTTPEngine as http
+import ftp
+import sys
+import socket
+
+
+def multiserver():
+    while input("Want to scan?(?/n)\n")!="n":
+        ip = input("enter an ip addr\n")
+        service = input("HTTP/HTTPS/FTP\n")
+        if(service=="HTTP"):
+            http.getfromHTTP(ip,"HTTP")
+        elif(service=="HTTPS"):
+            http.getfromHTTP(ip,"HTTPS")
+        elif(service=="FTP"):
+            sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            sock.settimeout(4.0)
+            port = 21 #(FTP Control port)
+            try:
+                sock.connect((ip,port))
+                ftp.Probe(sock)
+                sock.close()
+            except TimeoutError:
+                print("timeout, server is offline\n")
+        
+
+    return
+
+
+
+
+
+if __name__ == "__main__":
+    multiserver()
